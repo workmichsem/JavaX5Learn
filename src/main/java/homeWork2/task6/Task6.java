7c288b1860532bf04b5bb7f834595b167aa2ff21
workmichsem <workmichsem@gmail.com>
2020-09-25 15:57:09 +0300

    Update Task6.java

--- src/main/java/homeWork2/task6/Task6.java
+++ src/main/java/homeWork2/task6/Task6.java
@@ -10,37 +10,23 @@ public class Task6 {
         Scanner scanner = new Scanner(System.in);
         try {
             inputNumber = scanner.nextInt();
-            return inputNumber;
         } catch (NumberFormatException e) {
             System.out.println("Введенное значение не соответствует условиям. Программа будет прервана.");
             System.exit(0);
-            return inputNumber;
         }
-    }
-
-    public static int сhekValue() {
-        int parseNumber = inputValueFromScreen();
-        try {
-            if (parseNumber > Integer.MAX_VALUE || parseNumber < Integer.MIN_VALUE) {
-                System.out.println("Введенное значение не соответствует условиям. Программа будет прервана.");
-                System.exit(0);
-            }
-            return parseNumber;
-        } catch (NumberFormatException e) {
-            System.out.println("Введенное значение не соответствует условиям. Программа будет прервана.");
-            System.exit(0);
-            return parseNumber;
-        }
+        return inputNumber;
     }
 
     public static void numberProcessing() {
-        if (сhekValue() > 0) {
+        int parseNumber = inputValueFromScreen();
+        if (parseNumber > 0) {
             System.out.println("число положительное");
-        } else if (сhekValue() < 0) {
+        } else if (parseNumber < 0) {
             System.out.println("число отрицательное");
-        } else if (сhekValue() == 0) {
+        } else if (parseNumber == 0) {
             System.out.println("Введенное значение равно нулю.");
-        } else if (сhekValue() % 2 == 0) {
+        }
+        if (parseNumber % 2 == 0) {
             System.out.println("Введенное значение четное");
         } else {
             System.out.println("Введенное значение не четное");
